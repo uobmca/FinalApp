d64a67a17739f16fb074d30049ce8f3c127c3502
Marco Saia <marco.saia@xorovo.com>
2019-05-06 16:40:20 +0200

    Redesign / Toolbar item in incomes and expenses page / Bug fixes / Google Sign-In for Android

---
+++ FinalApp.iOS/Renderers/NoBounceListViewRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FinalApp.iOS.Renderers;
+using FinalApp.Views.Base;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(FinalApp.Views.Base.NoBounceListView), typeof(ListViewRenderer))]
+namespace FinalApp.iOS.Renderers {
+    public class NoBounceListView : ListViewRenderer {
+
+        public NoBounceListView() {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e) {
+            base.OnElementChanged(e);
+            DisableBounce();
+        }
+
+        private void DisableBounce() {
+            if (Element != null) {
+                Control.AlwaysBounceVertical = false;
+                Control.Bounces = false;
+            }
+        }
+    }
+}
---
+++ FinalApp/Views/Base/NoBounceListView.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FinalApp.Views.Base {
+    public class NoBounceScrollView : ScrollView {
+
+    }
+}
